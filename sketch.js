08f95891278f1b45c78cbd1ee48d5a500beb71e8
Sergey Alyaev <saly@norceresearch.no>
2025-01-21 23:12:43 +0100

    Added coding train version

--- sketch.js
+++ sketch.js
@@ -76,6 +76,56 @@ function mouseDragged() {
   }
 }
 
+function getNextGridCodingTrain(grid){
+  // Create a 2D array for the next frame of animation
+  let nextGrid = make2DArray(cols, rows);
+
+  // Check every cell
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows ; j++) {
+      // What is the state?
+      let state = grid[i][j];
+      
+      // If it's a piece of sand!
+      if (state > 0) {
+        // What is below?
+        let below = grid[i][j + 1];
+        
+        // Randomly fall left or right
+        let dir = 1;
+        if (random(1) < 0.5) {
+          dir *= -1;
+        }
+        
+        // Check below left or right
+        let belowA = -1;
+        let belowB = -1;
+        if (withinCols(i + dir)) {
+          belowA = grid[i + dir][j + 1];
+        }
+        if (withinCols(i - dir)) {
+          belowB = grid[i - dir][j + 1];
+        }
+        
+        
+        // Can it fall below or left or right?
+        if (below === 0) {
+          nextGrid[i][j + 1] = state;
+        } else if (belowA === 0) {
+          nextGrid[i + dir][j + 1] = state;
+        } else if (belowB === 0) {
+          nextGrid[i - dir][j + 1] = state;
+        // Stay put!
+        } else {
+          nextGrid[i][j] = state;
+        }
+      }
+    }
+  }
+  return nextGrid;
+}
+
+
 function getNextGridConservative(grid){
   // Create a 2D array for the next frame of animation
   let nextGrid = make2DArray(cols, rows);
